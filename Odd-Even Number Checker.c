3ef2be9825957f5f2428ee89cea8d4aa583f0716
shantanu <i.am.shantanu07@gmail.com>
2025-08-25 17:33:39 +0530

    Add files via upload

---
+++ Odd-Even Number Checker.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    printf("Welcome to the Odd/Even Number Checker");
+    int number;
+printf("Enter an integer: ");
+scanf("%d", &number);
+if (number % 2 == 0) {
+    printf("%d is even.", number);
+} else {
+    printf("%d is odd.", number);
+}
+printf("Thankyou for using the Odd/Even Number Checker")
+    return 0;
+}
