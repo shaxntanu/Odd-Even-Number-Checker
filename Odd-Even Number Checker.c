f72d21a3933bde0bfee5c2101276e607344c2dc9
shantanu <i.am.shantanu07@gmail.com>
2025-08-25 17:46:44 +0530

    Update Odd-Even Number Checker.c

--- Odd-Even Number Checker.c
+++ Odd-Even Number Checker.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 
 int main(){
-    printf("Welcome to the Odd/Even Number Checker");
+    printf("Welcome to the Odd/Even Number Checker\n");
     int number;
-printf("Enter an integer: ");
+printf("Enter an integer:");
 scanf("%d", &number);
 if (number % 2 == 0) {
     printf("%d is even.", number);
 } else {
     printf("%d is odd.", number);
 }
-printf("Thankyou for using the Odd/Even Number Checker");
+printf("\nThankyou for using the Odd/Even Number Checker");
     return 0;
 
 }
+
